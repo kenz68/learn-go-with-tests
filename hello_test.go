3e31db933d9f9530196a3db77b43f8c59309435f
quocle <quoc.le@futa.vn>
2025-03-18 13:57:47 +0700

    refactor & update doc

--- hello_test.go
+++ hello_test.go
@@ -7,17 +7,22 @@ func TestHello(t *testing.T) {
 		got := Hello("Chris")
 		want := "Hello, Chris"
 
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
 		got := Hello("")
-		want := "Hello, World"
+		want := "Hello, World1"
 
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
+}
+
+func assertCorrectMessage(t *testing.T, got, want string) {
+	// t.Helper() is needed to tell the test suite that this method is a helper.
+	// By doing this when it fails the line number reported will be in our function call rather than inside our test helper.
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
 }
