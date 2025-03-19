0deea32f876264aec8adee6f10a59ef134951506
quocle <quoc.le@futa.vn>
2025-03-19 16:00:46 +0700

    do practice exercises iteration

--- iteration/repeat.go
+++ iteration/repeat.go
@@ -2,12 +2,10 @@ package iteration
 
 import "strings"
 
-const repeatCount = 5
-
-// Repeat returns a string with the character repeated 5 times
-func Repeat(character string) string {
+// Repeat returns a string with the character repeated n times
+func Repeat(character string, n int) string {
 	var repeated strings.Builder // its perfromance is better than using string concatenation from 136 ns/op to 23.91 ns/op
-	for i := 0; i < repeatCount; i++ {
+	for i := 0; i < n; i++ {
 		repeated.WriteString(character)
 	}
 	return repeated.String()
--- iteration/repeat_test.go
+++ iteration/repeat_test.go
@@ -1,18 +1,47 @@
 package iteration
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestRepeat(t *testing.T) {
-	repeated := Repeat("a")
-	expected := "aaaaa"
+	t.Run("repeat 5 times", func(t *testing.T) {
+		repeated := Repeat("a", 5)
+		expected := "aaaaa"
 
-	if repeated != expected {
-		t.Errorf("expected '%s' but got '%s'", expected, repeated)
-	}
+		if repeated != expected {
+			t.Errorf("expected '%s' but got '%s'", expected, repeated)
+		}
+	})
+
+	t.Run("repeat 3 times", func(t *testing.T) {
+		repeated := Repeat("a", 3)
+		expected := "aaa"
+
+		if repeated != expected {
+			t.Errorf("expected '%s' but got '%s'", expected, repeated)
+		}
+	})
+
+	t.Run("repeat 0 times", func(t *testing.T) {
+		repeated := Repeat("a", 0)
+		expected := ""
+
+		if repeated != expected {
+			t.Errorf("expected '%s' but got '%s'", expected, repeated)
+		}
+	})
 }
 
 func BenchmarkRepeat(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		Repeat("a")
+		Repeat("a", 5)
 	}
+}
+
+func ExampleRepeat() {
+	repeated := Repeat("a", 8)
+	fmt.Println(repeated)
+	// Output: aaaaaaaa
 }
