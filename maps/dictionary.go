322dc26c9744e3a9535d75507c75750380663768
quocle <quoc.le@futa.vn>
2025-03-24 15:12:16 +0700

    dic CRUD tests

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -2,6 +2,61 @@ package main
 
 type Dictionary map[string]string
 
-func (d Dictionary) Search(word string) string {
-	return d[word]
+const (
+	ErrNotFound         = DictionaryErr("could not find the word you were looking for")
+	ErrWordExist        = DictionaryErr("cannot add word because it already exists")
+	ErrWordDoesNotExist = DictionaryErr("cannot update word because it does not exist")
+)
+
+type DictionaryErr string
+
+func (e DictionaryErr) Error() string {
+	return string(e)
+}
+
+func (d Dictionary) Search(word string) (string, error) {
+	defination, ok := d[word]
+	if !ok {
+		return "", ErrNotFound
+	}
+	return defination, nil
+}
+
+func (d Dictionary) Add(word, defination string) error {
+	_, err := d.Search(word)
+	switch err {
+	case ErrNotFound:
+		d[word] = defination
+	case nil:
+		return ErrWordExist
+	default:
+		return err
+	}
+	return nil
+}
+
+func (d Dictionary) Update(word, defination string) error {
+	_, err := d.Search(word)
+	switch err {
+	case ErrNotFound:
+		return ErrWordDoesNotExist
+	case nil:
+		d[word] = defination
+	default:
+		return err
+	}
+	return nil
+}
+
+func (d Dictionary) Delete(word string) error {
+	_, err := d.Search(word)
+	switch err {
+	case ErrNotFound:
+		return ErrWordDoesNotExist
+	case nil:
+		delete(d, word)
+	default:
+		return err
+	}
+	return nil
 }
